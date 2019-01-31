ae7866358171552d60f33f3a2b539481d613fccb
vinamragrawal <vinamragrawal@gmail.com>
2019-01-31 21:28:32 +0000

    setup events in contract

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -11,20 +11,40 @@ contract RockPaperScissors {
     // Read/write Candidates
     mapping(uint => Candidate) public candidates;
 
-    string public candidate;
-
     // Store Candidates Count
     uint public candidatesCount;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
 
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+
     // Constructor
     constructor() public {
-       candidate = "Candidate 1";
-
       addCandidate("Candidate 1");
       addCandidate("Candidate 2");
     }
